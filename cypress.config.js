3399552ccbddf89b7ba18c26d7d178518ba3999a
Shakimova88 <zarinashakimova@gmail.com>
2024-05-26 22:15:19 -0400

    Install cypress-mochawesome-reporter and update configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,19 @@
-const { defineConfig } = require('cypress')
-require('dotenv').config()
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  chromeWebSecurity: false,
-  // retries: 2,
-
-  env : {
-    SITE_URL: process.env.UI_URL,
-    UI_USERNAME: process.env.UI_USERNAME,
-    UI_PASSWORD: process.env.UI_PASSWORD
-  },
-  
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
-      require('@cypress/grep/src/plugin')(config);
       return config;
     },
     baseUrl: 'https://techglobal-training.com',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-})
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
